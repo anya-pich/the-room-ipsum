44b6d4a526a1952151d3a77494ca2ab95deb7d0d
anya-pich <anna.pich@outlook.com>
2020-06-29 17:29:06 -0700

    unstyled user interface implemented

--- app.py
+++ app.py
@@ -1,12 +1,41 @@
-from flask import Flask
-from flask import request
+import json
+
+from flask import Flask, request, render_template, redirect, url_for, make_response, flash
+
+from jumbler import make_paragraphs
 
 app = Flask(__name__)
+app.secret_key = 'kla;sdkhweh.s,jfa;lkgheoith'
+
+def get_cookie():
+	try:
+		data = json.loads(request.cookies.get('settings'))
+	except TypeError:
+		data = {}
+	return data
 
 @app.route('/')
-@app.route('/<name>')
-def index(name="Johnny"):
-	name = request.args.get('name', name)
-	return "Oh hai there {}!".format(name)
+def index():
+	return render_template('index.html', cookie=get_cookie())
+
+@app.route('/text')
+def text():
+	data = get_cookie()
+	num = int(data['paragraphs'])
+	ipsum_text = make_paragraphs(num)
+	return render_template(
+		'text.html',
+		cookie=get_cookie(),
+		ipsum_text=ipsum_text
+	)
+
+@app.route('/save', methods=['POST'])
+def save():
+	flash("Here you go:")
+	response = make_response(redirect(url_for('text')))
+	data = get_cookie()
+	data.update(dict(request.form.items()))
+	response.set_cookie('settings', json.dumps(dict(request.form.items())))
+	return response
 
 app.run(debug=True, port=8000, host='0.0.0.0')
